018d203b43d27005f7b12bf313de00868666c826
fedichebbi <35900204+fedichebbi@users.noreply.github.com>
2023-04-14 14:41:42 +0100

    Implement StartupList component

--- src/Components/Startup/StartupList.tsx
+++ src/Components/Startup/StartupList.tsx
@@ -1,5 +1,54 @@
-import { Fragment, ReactElement } from "react";
+import { Grid, Pagination, Paper, Typography } from "@mui/material";
+import { ReactElement, useEffect, useState } from "react";
+import { StartupHttpService } from "../../Http/Startup/Startup.http.service";
+import { Startup } from "../../Types/Startup"
+
+const ITEMS_PAGINATION = 20
 
 export default function StartupList(): ReactElement {
-  return <Fragment></Fragment>;
+  const [startups, setStartups] = useState<Startup[]>([])
+  const [page, setPage] = useState(1)
+
+  useEffect(() => {
+    fetchStartups()
+  }, [])
+
+  async function fetchStartups() {
+    try {
+      const _startups = await StartupHttpService.getStartups()
+      setStartups(_startups)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  function caption(startup: Startup) {
+    const foundYear = startup?.dateFounded?.getFullYear()
+    return `Founded: ${foundYear} | ${startup.employees} Employees | $ ${startup.totalFunding} Mio. | ${startup.currentInvestmentStage}`
+  }
+
+  function handleChange(_: any, page: number) {
+    setPage(page)
+  }
+
+  if (!startups) return (<></>);
+  
+  const itemsCount = (page - 1) * ITEMS_PAGINATION
+
+  return (
+    <div id="startup-list">
+      <Typography>Page: {page}</Typography>
+      <Pagination count={10} page={page} onChange={handleChange} />
+      <Grid id="startup-list" container >
+        {startups.slice(itemsCount, itemsCount + 20).map(startup => (
+          <Grid item width="100%" key={startup.id}>
+            <Paper sx={{ padding: '.75rem', marginBottom: '.5rem' }} >
+              <Typography variant="h5">{startup.name}</Typography>
+              <Typography variant="caption">{caption(startup)}</Typography>
+              <Typography sx={{ marginTop: '0.5rem' }}>{startup.shortDescription}</Typography>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </div>);
 }
